cd9ccb183550300622ea882cce3e60f346804b95
Viliyan <viliyantrbr@gmail.com>
2018-05-03 21:54:35 +0300

    Include "Vibrator" in "Read" & "Host" Threads

--- app/src/main/java/com/viliyantrbr/nfcemvpayer/thread/ReadPaycardThread.java
+++ app/src/main/java/com/viliyantrbr/nfcemvpayer/thread/ReadPaycardThread.java
@@ -3,6 +3,9 @@ package com.viliyantrbr.nfcemvpayer.thread;
 import android.content.Context;
 import android.nfc.Tag;
 import android.nfc.tech.IsoDep;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.annotation.NonNull;
 
 import com.viliyantrbr.nfcemvpayer.R;
@@ -45,6 +48,40 @@ public class ReadPaycardThread implements Runnable {
             LogUtil.e(TAG, e.toString());
 
             e.printStackTrace();
+        }
+
+        Vibrator vibrator = null;
+        try {
+            vibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        } catch (Exception e) {
+            LogUtil.e(TAG, e.getMessage());
+            LogUtil.e(TAG, e.toString());
+
+            e.printStackTrace();
+        }
+
+        if (vibrator != null) {
+            long vibeTime = 400; // milliseconds
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                try {
+                    vibrator.vibrate(VibrationEffect.createOneShot(vibeTime, VibrationEffect.DEFAULT_AMPLITUDE));
+                } catch (Exception e) {
+                    LogUtil.e(TAG, e.getMessage());
+                    LogUtil.e(TAG, e.toString());
+
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    vibrator.vibrate(vibeTime);
+                } catch (Exception e) {
+                    LogUtil.e(TAG, e.getMessage());
+                    LogUtil.e(TAG, e.toString());
+
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
