b611eff96d9286df532b1b9404673a7c062ada26
Viliyan <viliyantrbr@gmail.com>
2018-05-03 21:52:59 +0300

    Include "Vibrator" in "Read" & "Host" Threads

--- app/src/main/java/com/viliyantrbr/nfcemvpayer/thread/HostPaycardThread.java
+++ app/src/main/java/com/viliyantrbr/nfcemvpayer/thread/HostPaycardThread.java
@@ -1,6 +1,9 @@
 package com.viliyantrbr.nfcemvpayer.thread;
 
 import android.content.Context;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.support.annotation.NonNull;
 
 import com.viliyantrbr.nfcemvpayer.util.LogUtil;
@@ -8,8 +11,44 @@ import com.viliyantrbr.nfcemvpayer.util.LogUtil;
 public class HostPaycardThread implements Runnable {
     private static final String TAG = HostPaycardThread.class.getSimpleName();
 
-    public HostPaycardThread(@NonNull Context context) {
+    private Context mContext;
 
+    public HostPaycardThread(@NonNull Context context) {
+        mContext = context;
+
+        Vibrator vibrator = null;
+        try {
+            vibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        } catch (Exception e) {
+            LogUtil.e(TAG, e.getMessage());
+            LogUtil.e(TAG, e.toString());
+
+            e.printStackTrace();
+        }
+
+        if (vibrator != null) {
+            long vibeTime = 400; // milliseconds
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                try {
+                    vibrator.vibrate(VibrationEffect.createOneShot(vibeTime, VibrationEffect.DEFAULT_AMPLITUDE));
+                } catch (Exception e) {
+                    LogUtil.e(TAG, e.getMessage());
+                    LogUtil.e(TAG, e.toString());
+
+                    e.printStackTrace();
+                }
+            } else {
+                try {
+                    vibrator.vibrate(vibeTime);
+                } catch (Exception e) {
+                    LogUtil.e(TAG, e.getMessage());
+                    LogUtil.e(TAG, e.toString());
+
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
